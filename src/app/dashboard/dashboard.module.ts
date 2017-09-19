2d3f3bd7b8abe96bdda548dbf84b97db66fc08ef
THBar <choreno@gmail.com>
2017-09-19 17:15:18 -0400

    adding top nav

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,17 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import {DashboardComponent} from './dashboard.component';
 import {TopnavComponent} from '../shared/topnav/topnav.component';
+import {SidebarComponent} from '../shared/sidebar/sidebar.component';     
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule
   ],
-  declarations: [DashboardComponent, TopnavComponent]
+  declarations: [
+    DashboardComponent, 
+    TopnavComponent,
+    SidebarComponent
+  ]
 })
 export class DashboardModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopnavComponent } from './topnav/topnav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TopnavComponent]
+  declarations: [
+    TopnavComponent, 
+    SidebarComponent
+  ]
 })
 export class SharedModule { }
